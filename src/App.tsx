a8185f502d7edbc7208a06cb769840a6e776c455
Gabriel Rodrigues <52265978+ApenasGabs@users.noreply.github.com>
2024-04-14 21:48:13 +0000

    Adicionado teste na api do github

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [userData, setUserData] = useState<any>()
+const endPointUrl = 'https://api.github.com/users/apenasgabs/repos'
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(endPointUrl)
+        setUserData(result)
+      } catch (error) {
+        
+      }
+    }
+    fetchData()
+  },[])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {userData.map((repos: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined)=> {
+        return <div>{repos}</div>
+      })}
     </>
   )
 }
