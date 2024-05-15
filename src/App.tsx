bf96a18ca495f2c9803c2081f2e74e84a966c851
Gabriel Rodrigues <gabers357@gmail.com>
2024-05-15 01:10:15 -0300

    feat: Add copyright notice to footer in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ const App = () => {
     <>
       <Navbar />
       <Home />
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>Copyright © 2024 - All right reserved by ApenasGabs</p>
+        </aside>
+      </footer>
     </>
   );
 };
