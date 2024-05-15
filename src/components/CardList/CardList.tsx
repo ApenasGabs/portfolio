10338cd7149416245f5984692348332ec4124a2e
Gabriel Rodrigues <gabers357@gmail.com>
2024-05-15 01:51:24 -0300

    refactor: Remove unused imports and update CardList component in Home page

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -6,13 +6,17 @@ const CardList = ({ repoList }: CardListProps) => {
   return repoList.map((repo) => {
     const language = repo.language ?? "";
     const imgUrl = `https://skillicons.dev/icons?i=${language}`;
-    if (imgUrl) {
+    if (!imgUrl) {
       return (
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
+        <div className="card w-96 bg-primary  shadow-xl image-full">
           <figure>
             <img src={imgUrl} alt={repo.language} />
           </figure>
           <div className="card-body">
+            <h2 className="card-title">
+              Projeto <a href={repo.html_url}>{repo.name}</a>
+            </h2>
+            <p>{repo.description}</p>
             <p>
               Ultima atualização:
               {new Date(repo.updated_at).toLocaleString()}
@@ -21,15 +25,8 @@ const CardList = ({ repoList }: CardListProps) => {
               Ultimo push:
               {new Date(repo.pushed_at).toLocaleString()}
             </p>
-            <h2 className="card-title">
-              Criado em: {new Date(repo.created_at).toLocaleString()}
-            </h2>
-            <p>{repo.description}</p>{" "}
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">
-                <a href={repo.html_url}>{repo.name}</a>
-              </button>
-            </div>
+            Criado em: {new Date(repo.created_at).toLocaleString()}
+            <div className="card-actions justify-end"></div>
           </div>
         </div>
       );
@@ -37,11 +34,20 @@ const CardList = ({ repoList }: CardListProps) => {
     return (
       <div className="card w-96 bg-primary text-primary-content">
         <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
-          </div>
+          <h2 className="card-title">
+            Projeto <a href={repo.html_url}>{repo.name}</a>
+          </h2>
+          <p>{repo.description}</p>
+          <p>
+            Ultima atualização:
+            {new Date(repo.updated_at).toLocaleString()}
+          </p>
+          <p>
+            Ultimo push:
+            {new Date(repo.pushed_at).toLocaleString()}
+          </p>
+          Criado em: {new Date(repo.created_at).toLocaleString()}
+          <div className="card-actions justify-end"></div>
         </div>
       </div>
     );
